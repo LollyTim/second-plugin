602f1f0410d6470ede619d0958fce4f58f5e937f
Ololade Sanya <ololadetimileyin3@gmail.com>
2025-01-01 19:14:29 +0100

    concluded the plugin and it works really cool

--- code.ts
+++ code.ts
@@ -17,7 +17,61 @@ figma.ui.onmessage = async (msg) => {
       frameDirection,
       tintNumber,
     } = msg.formDataObj;
-    console.log(msg.formDataObj);
+
+    const parentFrame = figma.createFrame();
+    parentFrame.name = "Tints for the " + colorName + " color";
+    parentFrame.layoutMode = frameDirection.toUpperCase();
+    parentFrame.paddingLeft = 64;
+    parentFrame.paddingRight = 64;
+    parentFrame.paddingBottom = 64;
+    parentFrame.paddingTop = 64;
+    parentFrame.itemSpacing = parseInt(circleSpacing, 10);
+    parentFrame.primaryAxisSizingMode = "AUTO";
+    parentFrame.counterAxisSizingMode = "AUTO";
+
+    const hexToRgb = (
+      hex: string
+    ): { r: number; g: number; b: number } | null => {
+      let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result
+        ? {
+            r: parseInt(result[1], 16) / 255,
+            g: parseInt(result[2], 16) / 255,
+            b: parseInt(result[3], 16) / 255,
+          }
+        : null;
+    };
+
+    for (let i = 0; i < tintNumber; i++) {
+      const tintNode = figma.createEllipse();
+      tintNode.name = `${colorName} ${100 - i * 10}`; // Corrected naming
+      tintNode.resize(parseInt(circleSize, 10), parseInt(circleSize, 10));
+
+      const rgb = hexToRgb(colorCode);
+      if (rgb) {
+        // Apply tint effect
+
+        tintNode.fills = [
+          {
+            type: "SOLID",
+            color: {
+              r: rgb.r,
+              g: rgb.g,
+              b: rgb.b,
+            },
+          },
+        ];
+        // Add the node to the frame
+      }
+      tintNode.opacity = (100 - i * 10) / 100;
+      parentFrame.appendChild(tintNode);
+      // select and zoom to the generate frame
+      const selectFrame: FrameNode[] = [];
+      selectFrame.push(parentFrame);
+      figma.currentPage.selection = selectFrame;
+      figma.viewport.scrollAndZoomIntoView(selectFrame);
+    }
+
     console.log("Generating action detected");
     figma.closePlugin("Tint Generated Successfully");
   } else if (msg.type === "actionExit") {
